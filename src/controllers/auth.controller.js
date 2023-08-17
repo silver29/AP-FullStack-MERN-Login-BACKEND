d77d3fecd1a748db81a6bf083b43203587bdd2be
becquerk29 <becquerk29@gmail.com>
2023-08-16 22:44:19 -0500

    Backend sin el archivo config.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import { createAccessToken } from "../libs/jwt.js"
 import jwt from 'jsonwebtoken'
-import {TOKEN_SECRET} from '../config.js'
 
 export const register = async (req,res) => {
     const { email, password, username } = req.body
@@ -108,7 +107,7 @@ export const verifyToken = async (req,res) => {
 
     if(!token) return res.status(401).json({ message:"Unauthorized" });
 
-    jwt.verify(token,TOKEN_SECRET, async (err,user) => {
+    jwt.verify(token,process.env.TOKEN_SECRET, async (err,user) => {
         if(err) return res.status(401).json({ message:"Unauthorized" });
 
         const userFound = await User.findById(user.id)
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken'
-import { TOKEN_SECRET } from '../config.js'
 
 export const authRequired = (req, res, next) => {
     //console.log('validing token')
@@ -21,7 +20,7 @@ export const authRequired = (req, res, next) => {
         if(!token)
             return res.status(401).json({ message:"No token, authorization denied" });
         
-        jwt.verify(token, TOKEN_SECRET,(err,user) => {
+        jwt.verify(token, process.env.TOKEN_SECRET,(err,user) => {
             if(err) return res.status(403).json({ message:"Invalid token" });
             //console.log(user);
             /*req user es la petición de que está llegando y se guarda ahí, cuando se llame a req se puede acceder luego a la información que se ha guardado ahí*/
